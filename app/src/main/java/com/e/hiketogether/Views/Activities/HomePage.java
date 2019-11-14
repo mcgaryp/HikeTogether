e6c9c3c185264403575b5bcec183b99d00cb8e1b
hannahschanner <hannahschanner@gmail.com>
2019-11-14 12:13:25 -0700

    addedBackground

---
+++ app/src/main/java/com/e/hiketogether/Views/Activities/HomePage.java
@@ -0,0 +1,14 @@
+package com.e.hiketogether.Views.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
