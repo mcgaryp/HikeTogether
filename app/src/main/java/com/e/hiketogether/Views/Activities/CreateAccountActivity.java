3e8978061bb0c32ea094c69cb2d9133a34b5b3eb
mcgaryp <mcg15003@byui.edu>
2019-11-13 19:49:44 -0700

    add toast

--- app/src/main/java/com/e/hiketogether/Views/Activities/CreateAccountActivity.java
+++ app/src/main/java/com/e/hiketogether/Views/Activities/CreateAccountActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -41,20 +42,18 @@ public class CreateAccountActivity extends AppCompatActivity {
         editText = findViewById(R.id.createUsername);
         // Check to make sure username is within constraints
         try {
-            accountManager.checkUsername(editText, "username");
+            accountManager.checkUsername(editText, "Username");
         } catch (Exception e) {
             Log.d(TAG, e.getMessage());
             return;
         }
         // reset the setError
         editText.setError(null);
-
         // Set username
         username = editText.toString();
 
-        // Get the passwords to check them by
+        // point to the first password
         editText = findViewById(R.id.createPassword);
-
         // Check constraints
         try {
             accountManager.checkPassword(editText, "Password");
@@ -62,10 +61,12 @@ public class CreateAccountActivity extends AppCompatActivity {
             Log.d(TAG, e.getMessage());
             return;
         }
-        // Set password to not empty string
+        // reset setError
+        editText.setError(null);
+        // Set password
         password = editText.toString();
 
-        // Set the edit to next password
+        // point to verifyPassword
         editText = findViewById(R.id.createVerifyPassword);
         // Check constraints
         try {
@@ -76,23 +77,12 @@ public class CreateAccountActivity extends AppCompatActivity {
         }
         // reset the setError
         editText.setError(null);
-
-        // Set password
+        // Set SeccondPassword
         secondPassword = editText.toString();
-
-        // Check to make sure the two passwords match
-        try {
-            accountManager.crossCheckPasswords(password, secondPassword);
-        } catch (Exception e) {
-            Log.d(TAG, e.getMessage());
-            return;
-        }
-        // reset the setError
-        editText.setError(null);
 
         // Set the edit text to email
         editText = findViewById(R.id.createEmail);
-        // Check to make sure email is within constraints
+        // Check constraints
         try {
             accountManager.checkEmail(editText, "email");
         } catch (Exception e) {
@@ -101,9 +91,19 @@ public class CreateAccountActivity extends AppCompatActivity {
         }
         // reset the setError
         editText.setError(null);
-
         // Set email
         email = editText.toString();
+
+        // Check to make sure the two passwords match
+        try {
+            accountManager.crossCheckPasswords(password, secondPassword);
+        } catch (Exception e) {
+            Log.d(TAG, e.getMessage());
+            new Toast(getApplicationContext()).makeText(getApplicationContext(),"Pass words do not match", Toast.LENGTH_LONG).show();
+            return;
+        }
+        // reset the setError
+        editText.setError(null);
 
         // Create the account
         accountManager.createAccount(username, password, email);
