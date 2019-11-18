35db51af56110c89800c7be55e2b576352959726
AdamApplegate <56270346+AdamApplegate@users.noreply.github.com>
2019-11-18 16:41:05 -0700

    Quality of Life Visual Changes, added beginning toolbar functionality

--- app/src/main/java/com/e/hiketogether/Views/Activities/HomeActivity.java
+++ app/src/main/java/com/e/hiketogether/Views/Activities/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import com.e.hiketogether.Models.Account;
 import com.e.hiketogether.R;
@@ -25,6 +26,26 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+//        if (savedInstanceState == null) {
+//            // Let's first dynamically add a fragment into a frame container
+//            getSupportFragmentManager().beginTransaction().
+//                    replace(R.id.menuBar_frameLayout, new MenuBarFragment(), "MENU_BAR").
+//                    commit();
+//            // Now later we can lookup the fragment by tag
+//            MenuBarFragment menuBar = (MenuBarFragment)
+//                    getSupportFragmentManager().findFragmentByTag("MENU_BAR");
+//        }
     }
 
     //When user clicks "Login", this function will create the Activity and receive the user's account
