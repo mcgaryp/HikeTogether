9a6504e9bf502368bfa1c19ed6113538003e1b6b
AdamApplegate <56270346+AdamApplegate@users.noreply.github.com>
2019-11-18 17:24:38 -0700

    Extended toolbar functionality

--- app/src/main/java/com/e/hiketogether/Views/Activities/HomeActivity.java
+++ app/src/main/java/com/e/hiketogether/Views/Activities/HomeActivity.java
@@ -27,16 +27,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        View decorView = getWindow().getDecorView();
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-
-
 //        if (savedInstanceState == null) {
 //            // Let's first dynamically add a fragment into a frame container
 //            getSupportFragmentManager().beginTransaction().
