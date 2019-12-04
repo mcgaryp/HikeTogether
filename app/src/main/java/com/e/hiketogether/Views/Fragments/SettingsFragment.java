196e9975ec0f7631755735fca66e622fc33949df
mcgaryp <mcg15003@byui.edu>
2019-12-04 14:58:25 -0700

    settings

--- app/src/main/java/com/e/hiketogether/Views/Fragments/SettingsFragment.java
+++ app/src/main/java/com/e/hiketogether/Views/Fragments/SettingsFragment.java
@@ -66,14 +66,14 @@ public class SettingsFragment extends Fragment {
         }
         Log.d(TAG, "Account " + username + " received");
 
-        Spinner spinner = getActivity().findViewById(R.id.backgroundSpinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.backgroundArray, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+//        Spinner spinner = getActivity().findViewById(R.id.backgroundSpinner);
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+//                R.array.backgroundArray, android.R.layout.simple_spinner_item);
+//        // Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner
+//        spinner.setAdapter(adapter);
     }
 
     @Override
