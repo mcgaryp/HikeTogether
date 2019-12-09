1c3eaf80be70a8372c31bd4f6909d5fa5f65055a
mcgaryp <mcg15003@byui.edu>
2019-12-09 14:57:19 -0700

    yep

---
+++ app/src/main/java/com/e/hiketogether/Presenters/Adapters/SettingsAdapter.java
@@ -0,0 +1,4 @@
+package com.e.hiketogether.Presenters.Adapters;
+
+public class SettingsAdapter {
+}
--- app/src/main/java/com/e/hiketogether/Presenters/Managers/TrailManager.java
+++ app/src/main/java/com/e/hiketogether/Presenters/Managers/TrailManager.java
@@ -80,21 +80,21 @@ public class TrailManager {
         fManager = new FileManager(context);
 
         //See if trail is currently cached
-        if (!fManager.isCacheEmpty() && fManager.isQueryCached(url)) {
-            Log.d(TAG, "Retrieved query (" + url + ") from cache");
-            Toast.makeText(context, "Retrieved trails from cache", Toast.LENGTH_LONG).show();
-            return fManager.getCachedQuery(url);
-        }
+//        if (!fManager.isCacheNull() && fManager.isQueryCached(url)) {
+//                Log.d(TAG, "Retrieved query (" + url + ") from cache");
+//                Toast.makeText(context, "Retrieved trails from cache", Toast.LENGTH_LONG).show();
+//                return fManager.getCachedQuery(url);
+//        }
         //It wasn't cached, get it from the internet and cache it, then return it
-        else if (isNetworkAvailable()) {
+        if (isNetworkAvailable()) {
             Log.d(TAG, "Successfully connected to the internet");
             String trailList = new TrailHTTPHelper().execute(url).get();
 
             //Add new trail list to the cache
-            fManager.addToCache(url, trailList);
+            //fManager.addToCache(url, trailList);
             Log.d(TAG, "Added trails to the cache");
             //Write the cache to the phone since we updated it
-            fManager.writeCache(trailList);
+            //fManager.writeCache(trailList);
 
             return trailList;
         }
