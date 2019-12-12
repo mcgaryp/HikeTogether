df8fb21d5cbc6bc1638432409a1419979119537d
mcgaryp <mcg15003@byui.edu>
2019-12-12 12:02:57 -0700

    Create FavImageView.java

---
+++ app/src/main/java/com/e/hiketogether/Presenters/Helpers/FavImageView.java
@@ -0,0 +1,41 @@
+package com.e.hiketogether.Presenters.Helpers;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.e.hiketogether.R;
+
+public class FavImageView extends androidx.appcompat.widget.AppCompatImageView {
+    // VARIABLES
+    private final static String TAG = "FAV_IMAGE_VIEW";
+    private boolean isFavorite;
+
+    // Constructors
+    public FavImageView(Context context) {
+        super(context);
+        isFavorite = false;
+    }
+
+    public FavImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        isFavorite = false;
+    }
+
+    public FavImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        isFavorite = false;
+    }
+
+    // Change to favorite or not
+    public void change() {
+        // If not favorite
+        if (isFavorite == false) {
+            setImageResource(R.drawable.ic_star_border_black_24dp);
+            isFavorite = true;
+        }
+        else {
+            setImageResource(R.drawable.ic_star_yellow_50dp);
+            isFavorite = false;
+        }
+    }
+}
